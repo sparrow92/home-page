2746a60b6b434d865237c7e638378a5aaf11bd74
Piotr Wróbel <piotr.wrobel@hotmail.com>
2024-10-17 13:30:44 +0200

    Implement Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div style="display: flex; justify-content: center;">
-    <h1>Strona w budowie...</h1>
+    <h1 class="text-3xl font-bold underline">Strona w budowie...</h1>
   </div>
 </template>
