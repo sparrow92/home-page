a4e45480caca09392189115da625933969d9e94c
Piotr Wróbel <piotr.wrobel@hotmail.com>
2024-10-17 12:25:10 +0200

    Modify home page

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div style="display: flex; justify-content: center;">
+    <h1>Strona w budowie...</h1>
   </div>
 </template>
